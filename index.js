159aca548d15527e8750616076093aa969129b15
ᴠɪᴘᴇʀ-x <88338865+Viper-x0@users.noreply.github.com>
2024-01-18 16:16:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    app.get("/home", (req, res) => {
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname+"/kop/index.html");
-});
-    res.write('ASWIN SPARKY API');
-    res.end();
-}).listen(process.env.PORT || 3000);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
