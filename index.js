23e56719b1cd34409ace3cc9f4a188f190c93e9c
ruemods <105858358+ruemods@users.noreply.github.com>
2024-01-18 16:08:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
+    app.get("/", (req, res) => {
+  res.sendFile(__dirname+"/kop/index.html");
+});
     res.write('ASWIN SPARKY API');
     res.end();
 }).listen(process.env.PORT || 3000);
